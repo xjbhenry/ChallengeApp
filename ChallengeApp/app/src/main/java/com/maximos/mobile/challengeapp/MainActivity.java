f0ffcc873d440166a58a2f66069ddd3e3e745788
ankur-maximos <ankurchaudhry.india@gmail.com>
2014-10-04 01:59:13 -0400

    Committing login activity - Commit 2

--- ChallengeApp/app/src/main/java/com/maximos/mobile/challengeapp/MainActivity.java
+++ ChallengeApp/app/src/main/java/com/maximos/mobile/challengeapp/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
