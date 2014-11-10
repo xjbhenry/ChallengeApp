4571626e3739474f765a2ae1a6726747407d5f7d
xjbhenry <xjbhenry@gmail.com>
2014-11-09 23:20:43 -0500

    added Challenge Sub Page

--- ChallengeApp/app/src/main/java/com/maximos/mobile/challengeapp/feedpageproject/ProfileSubActivity.java
+++ ChallengeApp/app/src/main/java/com/maximos/mobile/challengeapp/feedpageproject/ProfileSubActivity.java
@@ -1,14 +1,103 @@
 package com.maximos.mobile.challengeapp.feedpageproject;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.maximos.mobile.challengeapp.R;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Henry on 11/9/2014.
  */
 public class ProfileSubActivity extends Activity {
+    public TextView songName, startTimeField, endTimeField;
+    private MediaPlayer mediaPlayer;
+    private double startTime = 0;
+    private double finalTime = 0;
+    private Handler myHandler = new Handler();
+    private SeekBar seekbar;
+    private ImageButton playButton, pauseButton;
+    public static int oneTimeOnly = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_sub);
+        startTimeField = (TextView) findViewById(R.id.textView1);
+        seekbar = (SeekBar) findViewById(R.id.seekBar1);
+        playButton = (ImageButton) findViewById(R.id.imageButton1);
+        pauseButton = (ImageButton) findViewById(R.id.imageButton2);
+        //songName.setText("One");
+        mediaPlayer = MediaPlayer.create(this, R.raw.one);
+        seekbar.setClickable(false);
+        pauseButton.setEnabled(false);
+
+    }
+
+    public void play(View view) {
+        Toast.makeText(getApplicationContext(), "Playing sound",
+                Toast.LENGTH_SHORT).show();
+        mediaPlayer.start();
+        finalTime = mediaPlayer.getDuration();
+        startTime = mediaPlayer.getCurrentPosition();
+        if (oneTimeOnly == 0) {
+            seekbar.setMax((int) finalTime);
+            oneTimeOnly = 1;
+        }
+
+       /* endTimeField.setText(String.format("%d min, %d sec",
+                        TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                        TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                        toMinutes((long) finalTime)))
+        );*/
+        startTimeField.setText(String.format("%d min, %d sec",
+                        TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                        TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                        toMinutes((long) startTime)))
+        );
+        seekbar.setProgress((int) startTime);
+        myHandler.postDelayed(UpdateSongTime, 100);
+        pauseButton.setEnabled(true);
+        playButton.setEnabled(false);
+    }
+
+    private Runnable UpdateSongTime = new Runnable() {
+        public void run() {
+            startTime = mediaPlayer.getCurrentPosition();
+            startTimeField.setText(String.format("%d min, %d sec",
+                            TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                            TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                            toMinutes((long) startTime)))
+            );
+            seekbar.setProgress((int) startTime);
+            myHandler.postDelayed(this, 100);
+        }
+    };
+
+    public void pause(View view) {
+        Toast.makeText(getApplicationContext(), "Pausing sound",
+                Toast.LENGTH_SHORT).show();
+
+        mediaPlayer.pause();
+        pauseButton.setEnabled(false);
+        playButton.setEnabled(true);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
